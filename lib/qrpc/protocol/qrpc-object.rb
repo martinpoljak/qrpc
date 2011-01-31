3adf2b59d633257795e3c2ee243eb47ed49def45
Martin Kozák <martinkozak@martinkozak.net>
2011-01-31 17:11:01 +0100

    protocol QRPC object

--- lib/qrpc/protocol/qrpc-object.rb
+++ lib/qrpc/protocol/qrpc-object.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
+require "json-rpc-objects/generic"
+require "json-rpc-objects/request"
 
 ##
 # General QRPC module.
@@ -14,17 +16,20 @@ module QRPC
     module Protocol
 
         ##
-        # QRPC JSON-RPC QRPC object.
+        # QRPC JSON-RPC QRPC object. Extends the 
+        # +JsonRpcObjects::Generic::Object+. See its documentation for
+        # additional methods.
+        #
         # @since 0.2.0
         #
         
-        class QrpcObject
+        class QrpcObject < JsonRpcObjects::Generic::Object
         
             ##
-            # Holds QRPC object options.
+            # Holds JSON-RPC version indication.
             #
             
-            @data
+            VERSION = JsonRpcObjects::Request::VERSION
             
             ##
             # Creates new QRPC JSON-RPC object.
@@ -35,27 +40,58 @@ module QRPC
             end
             
             ##
-            # Constructor.
+            # Checks correctness of the object data.
             #
             
-            def initialize(data)
+            def check!
+                if (not @priority.nil?) and not (@priority.kind_of? Numeric)
+                    raise Exception::new("Priority is expected to be Numeric.")
+                end
             end
-            
+                        
             ##
-            # Converts to JSON.
+            # Renders data to output form.
+            # @return [Hash] with data of object
             #
-            
-            def to_json
+
+            def output
+                result = { :version => "1.0" }
+                
+                if not @priority.nil?
+                    result[:priority] = @priority
+                end
+                
+                if not @client_id.nil?
+                    result[:client_id] = @client_id
+                end
+                
                 return result
             end
-                
+       
+
+            protected
+            
             ##
-            # Returns version identifier.
+            # Assigns data.
+            #
+
+            def data=(value, mode = nil)
+                data = __convert_data(value, mode)
+                
+                @priority = data[:priority]
+                @client_id = data[:client_id]
+            end
+
+            ##
+            # Converts data to standard (defined) format.
             #
             
-            def version
-                :"1.0"
+            def normalize!
+                if not @priority.nil?
+                    @priority = @priority.to_i
+                end
             end
+            
         end
                 
     end
