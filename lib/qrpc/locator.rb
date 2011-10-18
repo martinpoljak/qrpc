e6705c4d9b683a1519d24306fbd638540682596c
Martin Kozák <martinkozak@martinkozak.net>
2011-10-18 22:04:21 +0200

    compatibility layer for old locator

--- lib/qrpc/locator.rb
+++ lib/qrpc/locator.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 # (c) 2011 Martin Kozák (martinkozak@martinkozak.net)
 
+require "qrpc/locator/em-jack"
+
 ##
 # General QRPC module.
 #
@@ -12,94 +14,16 @@ module QRPC
     # @deprecated (since 0.9.0) in favour to locators for each queue types
     #
     
-    class Locator
-    
+    module Locator
+      
         ##
-        # Contains queue name.
-        # @return [Symbol]
+        # Returns new instance of {EMJackLocator}.
+        # @returns [EMJackLocator]
         #
-
-        attr_accessor :queue
-        @queue
-        
-        ##
-        # Contains host.
-        # @return [String]
-        #
-
-        attr_accessor :host
-        @host
-        
-        ##
-        # Contains port.
-        # @return [Integer]
-        #
-
-        attr_accessor :port
-        @port
-        
-        ##
-        # Parser.
-        #
-        
-        PARSER = /^(.+)@(.+)(?:\:(\d+))?$/
-        
-        ##
-        # Default port.
-        #
-        
-        DEFAULT_PORT = 11300
-        
-        ##
-        # Constructor.
-        #
-        # @param [String, Symbol] queue queue name
-        # @param [String] host host name
-        # @param [Integer] port port of the host
-        #
-        
-        def initialize(queue, host = "localhost", port = 11300)
-            @queue = queue.to_s
-            @host = host
-            @port = port
+         
+        def self.new(*args, &block)
+            EMJackLocator::new(*args, &block)
         end
-        
-        ##
-        # Parses the locator.
-        # Excpects form +<queue>@<host>:<port>+. Port is optional.
-        #
-        # @param [String] string locator in string form
-        # @return [QRPC::Locator] new instance
-        #
-        
-        def self.parse(string)
-            match = string.match(self::PARSER)
-            
-            queue = match[1]
-            host = match[2]
-            
-            if match.length == 3
-                port = match[3]
-            else
-                port = self::DEFAULT_PORT
-            end
-            
-            port = port.to_i
-            
-            ##
-            
-            return self::new(queue, host, port);
-        end
-        
-        ##
-        # Converts back to string.
-        # @return [String] locator in string form
-        #
-        
-        def to_s
-            @queue.dup << "@" << @host << ":" << @port.to_s
-        end
-        
     end
-    
+        
 end
